f2ec5aed2db9ad102efc9c33c7e08f7fc2eaa146
Nick Oostdyk <Nickbro9@gmail.com>
2020-09-26 21:35:21 -0400

    Added title screen, started working on movement script

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public void startGame() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame() {
+
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    void FixedUpdate() {
+
+        float leftRight = Input.GetAxis("Horizontal");
+
+        if (!Mathf.Approximately(leftRight, 0f))
+            leftRight = leftRight < 0f ? -1f : 1f;
+
+        Vector2 targetVelocity = new Vector2()
+
+    }
+
+}
